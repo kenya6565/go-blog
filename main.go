6eda9a3eb05b1f01e4f5c58d4de8065333763bbc
kenya6565 <ken.australia0605@gmail.com>
2022-12-21 03:24:23 +0900

    create HTML elements in server app and display it

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/flosch/pongo2"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+const templPath = "src/template/"
 
 // グローバル変数
 var e = createMux()
@@ -33,5 +37,25 @@ func createMux() *echo.Echo {
 
 func articleIndex(c echo.Context) error {
 	// ステータスコード 200 で、"Hello, World!" という文字列をレスポンス
-	return c.String(http.StatusOK, "Hello, World!")
+	// return c.String(http.StatusOK, "Hello, World!")
+
+	data := map[string]interface{}{
+		"Message": "Hello, World",
+		"Now": time.Now(),
+	}
+	return render(c, "article/index.html", data)
+}
+
+func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
+	// pongo2 を利用して、テンプレートファイルとデータから HTML を生成している
+	// 生成した HTML はバイトデータに変えてreturn
+	return pongo2.Must(pongo2.FromCache(templPath + file)).ExecuteBytes(data)
+}
+
+func render(c echo.Context, file string, data map[string]interface{}) error {
+	b, err := htmlBlob(file,data)
+	if err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	return c.HTMLBlob(http.StatusOK, b)
 }
