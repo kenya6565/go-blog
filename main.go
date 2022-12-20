2c8e0fceb488be43d9c96173e4d00779e3421de9
kenya6565 <ken.australia0605@gmail.com>
2022-12-21 02:34:29 +0900

    add comment out

--- main.go
+++ main.go
@@ -7,24 +7,31 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+// グローバル変数
 var e = createMux()
 
 func main() {
+	// / にアクセスした時にarticleIndex()が発火する
 	e.GET("/", articleIndex)
 
+	// Webサーバーをポート番号 8080 で起動する
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 func createMux() *echo.Echo {
-	e := echo.New()
+	// アプリケーションインスタンスを生成
+  e := echo.New()
 
-	e.Use(middleware.Recover())
-	e.Use(middleware.Logger())
-	e.Use(middleware.Gzip())
+  // アプリケーションに各種ミドルウェアを設定
+  e.Use(middleware.Recover())
+  e.Use(middleware.Logger())
+  e.Use(middleware.Gzip())
 
-	return e
+  // アプリケーションインスタンスを返却
+  return e
 }
 
 func articleIndex(c echo.Context) error {
+	// ステータスコード 200 で、"Hello, World!" という文字列をレスポンス
 	return c.String(http.StatusOK, "Hello, World!")
 }
