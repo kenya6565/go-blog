c937b354a671d5f0ca60150722f978a6a87e09eb
kenya6565 <ken.australia0605@gmail.com>
2022-12-22 00:43:21 +0900

    add edit and new and show pages

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/flosch/pongo2"
@@ -17,6 +18,10 @@ var e = createMux()
 func main() {
 	// / にアクセスした時にarticleIndex()が発火する
 	e.GET("/", articleIndex)
+	e.GET("/new", articleNew)
+	// :idはパラメーターで　任意の文字列を受け取る
+  e.GET("/:id", articleShow)
+  e.GET("/:id/edit", articleEdit)
 
 	// Webサーバーをポート番号 8080 で起動する
 	e.Logger.Fatal(e.Start(":8080"))
@@ -43,10 +48,44 @@ func articleIndex(c echo.Context) error {
 	// return c.String(http.StatusOK, "Hello, World!")
 
 	data := map[string]interface{}{
-		"Message": "Hello, World",
+		"Message": "Article Index",
 		"Now": time.Now(),
 	}
 	return render(c, "article/index.html", data)
+}
+
+func articleNew(c echo.Context) error {
+	data := map[string]interface{}{
+		"Message": "Article New",
+		"Now": time.Now(),
+	}
+	return render(c, "article/new.html", data)
+}
+
+func articleShow(c echo.Context) error {
+	// c.Param("id") でパスパラメータの :id の部分を抽出しています。
+	// 例えば http://localhost:8080/999 でアクセスがあった場合、c.Param("id") によって 999 が取り出せます。
+	// c.Param() で取り出した値は文字列型になるので、strconv パッケージの Atoi() 関数を使って数値型にキャストしています。
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	data := map[string]interface{}{
+		"Message": "Article Show",
+		"Now": time.Now(),
+		"ID": id,
+	}
+	return render(c, "article/show.html", data)
+}
+
+
+func articleEdit(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	data := map[string]interface{}{
+		"Message": "Article Edit",
+		"Now": time.Now(),
+		"ID": id,
+	}
+	return render(c, "article/edit.html", data)
+	
 }
 
 func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
